f9bf2a565c2ccb651389fc8c7d27c200c59a357e
Bushra Addlan <bushra.addlan@gmail.com>
2017-04-12 16:24:27 -0400

    Removed redundant files.

--- formio-master/src/actions/fields/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function(router) {
-  return {
-    signature: require('./signature')(router.formio),
-    password: require('./password')(router.formio),
-    email: require('./email')(router.formio)
-  };
-};
--- formio-master/src/templates/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var _ = require('lodash');
-
-module.exports = {
-  default: _.cloneDeep(require('./default.json')),
-  empty: _.cloneDeep(require('./empty.json'))
-};
