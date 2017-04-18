8163455f7d1d0ca9e025b5a2e3133599fca36cd9
Bushra Addlan <bushra.addlan@gmail.com>
2017-04-18 13:04:11 -0400

    Applied string replacement to Formio’s default config file to inject environment variables from Docker.

--- scripts/formio_setup.sh
+++ scripts/formio_setup.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-FORMIO_MONGO_DSN=${FORMIO_MONGO_DSN:-mongodb://localhost:27017/xyz}
-FORMIO_MONGO_SECRET=${FORMIO_MONGO_DSN:-changeme}
-FORMIO_JWT_SECRET=${FORMIO_MONGO_DSN:-changeme}
+FORMIO_MONGO_DSN=${FORMIO_MONGO_DSN:-FORMIO_MONGO_DSN}
+FORMIO_MONGO_SECRET=${FORMIO_MONGO_SECRET:-FORMIO_MONGO_SECRET}
+FORMIO_JWT_SECRET=${FORMIO_JWT_SECRET:-FORMIO_JWT_SECRET}
 
 cd /opt/formio/config
-sed -i 's/FORMIO_MONGO_DSN/'"$FORMIO_MONGO_DSN"'/g' default.json
-sed -i 's/FORMIO_MONGO_SECRET/'"$FORMIO_MONGO_SECRET"'/g' default.json
-sed -i 's/FORMIO_JWT_SECRET/'"$FORMIO_JWT_SECRET"'/g' default.json
+
+# Note: the use of `#` as a delimiter in the `sed` command below is because the env variable being substituted may contain slashes
+#sed -i 's#FORMIO_MONGO_DSN#'"$FORMIO_MONGO_DSN"'#g' default.json
+sed -i 's#"mongo": ".*"#"mongo": "'"$FORMIO_MONGO_DSN"'"#g' default.json
+
+#sed -i 's/FORMIO_MONGO_SECRET/'"$FORMIO_MONGO_SECRET"'/g' default.json
+sed -i 's/"mongoSecret": ".*"/"mongoSecret": "'"$FORMIO_MONGO_SECRET"'"/g' default.json
+
+#sed -i 's/FORMIO_JWT_SECRET/'"$FORMIO_JWT_SECRET"'/g' default.json
+sed -i 's/"secret": ".*"/"secret": "'"$FORMIO_JWT_SECRET"'"/g' default.json
 
 echo "========================================================================"
 echo "Formio MongoDB DSN: \"$FORMIO_MONGO_DSN\""
