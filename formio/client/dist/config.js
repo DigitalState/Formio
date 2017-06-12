b3d288346234496cd1a1012a555ae15a0142a8a0
Bushra Addlan <bushra.addlan@gmail.com>
2017-06-12 16:59:07 -0400

    Updated Client app’s page title, header styles and logo.

--- formio/client/dist/config.js
+++ formio/client/dist/config.js
@@ -1,7 +1,7 @@
-
+// var APP_URL = 'https://example.form.io';
+// var API_URL = 'https://api.form.io';
 var APP_URL = 'http://localhost:3001';
 var API_URL = 'http://localhost:3001';
-
 
 // Parse query string
 var query = {};
